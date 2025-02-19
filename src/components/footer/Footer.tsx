10d92427b601f4d41452713d5a1e83ead7f8a410
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2025-02-19 13:56:58 +0100

    Update src/components/footer/Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -175,7 +175,7 @@ import React, { FC, useState } from "react";
         {/* MOBILE layout */}
         <div className="md:hidden">
           
-        <div className="px-[5px] md:px-[8px] xl:px-[206px] py-[5px] md:py-[8px] xl:py-[38px]">
+        <div className="px-[29px] md:px-[68px] xl:px-[206px] py-[25px] md:py-[39px] xl:py-[38px]">
           {categories.slice(0, numberOfCategories).map((cat, i) => (
             <div key={i} className="mb-4">
               <h4 className={`mb-2 text-[#ffffff] text-[14px] md:text-[15px] xl:text-[20px] font-normal md:font-normal xl:font-semibold`}>{cat.name}</h4>
@@ -188,13 +188,13 @@ import React, { FC, useState } from "react";
           ))}
         </div>
       
-          <div className="border-[#616161] border-[1px] md:border-[1px] xl:border-[1px] w-[100%] md:w-[100%] xl:w-[80%] mx-auto my-4" />
+          <div className="border-[#616161] border-[1px] md:border-[1px] xl:border-[1px] w-[86%] md:w-[85%] xl:w-[80%] mx-auto my-4" />
           
-        <div className="px-[5px] md:px-[8px] xl:px-[189px] py-[5px] md:py-[8px] xl:py-[50px] flex justify-between items-center">
+        <div className="px-[27px] md:px-[60px] xl:px-[189px] py-[24px] md:py-[35px] xl:py-[50px] flex justify-between items-center">
           <div className="flex flex-col">
-            <img src="https://i.postimg.cc/PJf14dFD/logo-admin.png" alt="Logo" className="w-[60px] md:w-[80px] xl:w-[193px] mb-2" />
-            <span className="text-[#e3e3e3] text-[8px] md:text-[10px] xl:text-[16px]">{`Energaztech © ${new Date().getFullYear()}`}</span>
-            <span className="text-[#e3e3e3] text-[8px] md:text-[10px] xl:text-[16px]">{`Wszelkie prawa zastrzeżone.`}</span>
+            <img src="https://i.postimg.cc/PJf14dFD/logo-admin.png" alt="Logo" className="w-[119px] md:w-[154px] xl:w-[193px] mb-2" />
+            <span className="text-[#e3e3e3] text-[11px] md:text-[12px] xl:text-[16px] font-light md:font-light xl:font-normal">{`Energaztech © ${new Date().getFullYear()}`}</span>
+            <span className="text-[#e3e3e3] text-[11px] md:text-[12px] xl:text-[16px] font-light md:font-light xl:font-normal">{`Wszelkie prawa zastrzeżone.`}</span>
           </div>
           <div className="flex gap-[10px] md:gap-[10px] xl:gap-[10px]">
             {renderIcons()}
@@ -206,7 +206,7 @@ import React, { FC, useState } from "react";
         {/* TABLET layout */}
         <div className="hidden md:flex xl:hidden flex-col w-full">
           
-        <div className="px-[5px] md:px-[8px] xl:px-[206px] py-[5px] md:py-[8px] xl:py-[38px] w-full grid grid-cols-2 gap-8">
+        <div className="px-[29px] md:px-[68px] xl:px-[206px] py-[25px] md:py-[39px] xl:py-[38px] w-full grid grid-cols-2 gap-8">
           {categories.slice(0, numberOfCategories).map((cat, i) => (
             <div key={i}>
               <h4 className={`mb-2 text-[#ffffff] text-[14px] md:text-[15px] xl:text-[20px] font-normal md:font-normal xl:font-semibold`}>{cat.name}</h4>
@@ -219,16 +219,16 @@ import React, { FC, useState } from "react";
           ))}
         </div>
       
-          <div className="border-[#616161] border-[1px] md:border-[1px] xl:border-[1px] w-[100%] md:w-[100%] xl:w-[80%] mx-auto my-4" />
+          <div className="border-[#616161] border-[1px] md:border-[1px] xl:border-[1px] w-[86%] md:w-[85%] xl:w-[80%] mx-auto my-4" />
           
-        <div className="px-[5px] md:px-[8px] xl:px-[189px] py-[5px] md:py-[8px] xl:py-[50px] w-full relative flex justify-between items-center">
-          <img src="https://i.postimg.cc/PJf14dFD/logo-admin.png" alt="Logo" className="w-[60px] md:w-[80px] xl:w-[193px] object-contain" />
+        <div className="px-[27px] md:px-[60px] xl:px-[189px] py-[24px] md:py-[35px] xl:py-[50px] w-full relative flex justify-between items-center">
+          <img src="https://i.postimg.cc/PJf14dFD/logo-admin.png" alt="Logo" className="w-[119px] md:w-[154px] xl:w-[193px] object-contain" />
           <div className="absolute left-1/2 transform -translate-x-1/2 flex gap-[10px] md:gap-[10px] xl:gap-[10px]">
             {renderIcons()}
           </div>
           <div className="text-right flex flex-col">
-            <span className="text-[#e3e3e3] text-[8px] md:text-[10px] xl:text-[16px]">{`Energaztech © ${new Date().getFullYear()}`}</span>
-            <span className="text-[#e3e3e3] text-[8px] md:text-[10px] xl:text-[16px]">{`Wszelkie prawa zastrzeżone.`}</span>
+            <span className="text-[#e3e3e3] text-[11px] md:text-[12px] xl:text-[16px] font-light md:font-light xl:font-normal">{`Energaztech © ${new Date().getFullYear()}`}</span>
+            <span className="text-[#e3e3e3] text-[11px] md:text-[12px] xl:text-[16px] font-light md:font-light xl:font-normal">{`Wszelkie prawa zastrzeżone.`}</span>
           </div>
         </div>
       
@@ -237,7 +237,7 @@ import React, { FC, useState } from "react";
         {/* DESKTOP layout */}
         <div className="hidden xl:flex flex-col w-full">
           
-          <div className="px-[5px] md:px-[8px] xl:px-[206px] py-[5px] md:py-[8px] xl:py-[38px] w-full">
+          <div className="px-[29px] md:px-[68px] xl:px-[206px] py-[25px] md:py-[39px] xl:py-[38px] w-full">
             <div className="flex flex-wrap place-content-between gap-8">
               {categories.map((cat, i) => (
                 <div key={i} className="min-w-[140px]">
@@ -252,16 +252,16 @@ import React, { FC, useState } from "react";
             </div>
           </div>
         
-          <div className="border-[#616161] border-[1px] md:border-[1px] xl:border-[1px] w-[100%] md:w-[100%] xl:w-[80%] mx-auto my-4" />
+          <div className="border-[#616161] border-[1px] md:border-[1px] xl:border-[1px] w-[86%] md:w-[85%] xl:w-[80%] mx-auto my-4" />
           
-          <div className="px-[5px] md:px-[8px] xl:px-[189px] py-[5px] md:py-[8px] xl:py-[50px] w-full relative flex justify-between items-center">
-            <img src="https://i.postimg.cc/PJf14dFD/logo-admin.png" alt="Logo" className="w-[60px] md:w-[80px] xl:w-[193px] object-contain" />
+          <div className="px-[27px] md:px-[60px] xl:px-[189px] py-[24px] md:py-[35px] xl:py-[50px] w-full relative flex justify-between items-center">
+            <img src="https://i.postimg.cc/PJf14dFD/logo-admin.png" alt="Logo" className="w-[119px] md:w-[154px] xl:w-[193px] object-contain" />
             <div className="absolute left-1/2 transform -translate-x-1/2 flex gap-[10px] md:gap-[10px] xl:gap-[10px]">
               {renderIcons()}
             </div>
             <div className="text-right flex flex-col">
-              <span className="text-[#e3e3e3] text-[8px] md:text-[10px] xl:text-[16px]">{`Energaztech © ${new Date().getFullYear()}`}</span>
-              <span className="text-[#e3e3e3] text-[8px] md:text-[10px] xl:text-[16px]">{`Wszelkie prawa zastrzeżone.`}</span>
+              <span className="text-[#e3e3e3] text-[11px] md:text-[12px] xl:text-[16px] font-light md:font-light xl:font-normal">{`Energaztech © ${new Date().getFullYear()}`}</span>
+              <span className="text-[#e3e3e3] text-[11px] md:text-[12px] xl:text-[16px] font-light md:font-light xl:font-normal">{`Wszelkie prawa zastrzeżone.`}</span>
             </div>
           </div>
         
