0a4e73e40bf8eb3ca7a0e8a4c0788be19adef401
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2025-02-23 19:50:25 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,19 @@ const GeneratedPage: React.FC = () => {
   return (
     <MainLayout>
       
-<section className="h-[93.7vh] md:h-[186.8vh] xl:h-[92.4vh]" style={{ position: "relative", backgroundColor: "#dedede" }}>
-      <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[80.63347868217055%] md:w-[72.49110772357724%] xl:w-[35.052083333333336%] h-[13.402271518413066%] md:h-[11.82388637345708%] xl:h-[20.967741935483872%] left-[8.624031007751924%] md:left-[21.686991869918703%] xl:left-[5.833333333333333%] top-[35.771907366943445%] md:top-[28.289679822049372%] xl:top-[19.577308120133473%]" style={{ position: "absolute" }} />
-<div className="w-[61.62669573643411%] md:w-[53.583587398373986%] xl:w-[24.53125%] h-[9.429129576505254%] md:h-[6.688076623085298%] xl:h-[14.004711425206125%] left-[16.71269379844966%] md:left-[19.02693089430901%] xl:left-[59.27083333333337%] top-[11.380472209568133%] md:top-[8.812519165032267%] xl:top-[30.742049469964638%] absolute">
+<section className="h-[93.7vh] md:h-[118.1vh] xl:h-[92.4vh]" style={{ position: "relative", backgroundColor: "#dedede" }}>
+      <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[80.63347868217055%] md:w-[86.63744918699187%] xl:w-[37.552083333333336%] h-[13.402271518413066%] md:h-[25.187941672996253%] xl:h-[36.279873855389646%] left-[7.906976744186048%] md:left-[8.028455284552873%] xl:left-[3.697916666666668%] top-[80.25506648424889%] md:top-[10.062947417174282%] xl:top-[19.930665010592822%]" style={{ position: "absolute" }} />
+<div className="w-[85.79941860465117%] md:w-[82.51524390243902%] xl:w-[38.125%] h-[32.72794802657769%] md:h-[32.26783642820736%] xl:h-[50.73027090694936%] left-[5.953246124031039%] md:left-[8.619156504065119%] xl:left-[51.30208333333336%] top-[40.353110648723856%] md:top-[58.756956626747%] xl:top-[22.002355712603073%] absolute">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206252.80376311584!2d-115.33980749872859!3d36.12516446087101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80beb782a4f57dd1%3A0x3accd5e6d5b379a3!2sLas%20Vegas%2C%20Nevada%2C%20Stany%20Zjednoczone!5e0!3m2!1spl!2spl!4v1740334971548!5m2!1spl!2spl"
+      loading="lazy"
+      className="w-full h-full rounded-[0px] border-0"></iframe>
+  </div>
+<div className="w-[61.62669573643411%] md:w-[53.583587398373986%] xl:w-[17.239583333333332%] h-[9.429129576505254%] md:h-[6.688076623085298%] xl:h-[9.293286219081272%] left-[17.87548449612408%] md:left-[22.563516260162665%] xl:left-[12.7604166666667%] top-[22.646442012123295%] md:top-[41.076488310659144%] xl:top-[67.72673733804484%] absolute">
     <a href="/asdasd" className="bg-[#a81f1f] hover:bg-[#49bc86] text-[#7a8118] hover:text-[#917878] text-[49px] font-[700] rounded-[80px] border-[2px] border-[#161466] hover:border-[#1a471f] px-4 py-2 cursor-pointer w-full h-full flex items-center justify-center">
       CIPA
     </a>
   </div>
-<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[87.52543604651163%] md:w-[40.78125%] h-[15.090050481783294%] md:h-[26.195773081201335%] left-[4.4186046511627906%] md:left-[48.906250000000014%] top-[71.21283894735888%] md:top-[67.18576195773072%]" style={{ position: "absolute" }} />
     </section>
 <section className="h-[93.3vh] md:h-[50vh]" style={{ position: "relative", backgroundColor: "#971717" }}>
       
