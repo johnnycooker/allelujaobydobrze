14acff5b15949666d2f7ab48c635bef8fe32a564
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2025-02-23 01:11:22 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,12 @@ const GeneratedPage: React.FC = () => {
   return (
     <MainLayout>
       
-<section className="h-[224.4vh] md:h-[144.6vh] xl:h-[92.4vh]" style={{ position: "relative", backgroundColor: "#dedede" }}>
-      <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[35.052083333333336%] h-[20.967741935483872%] left-[5.833333333333333%] md:left-[51.68699186991867%] xl:left-[5.833333333333333%] top-[19.577308120133473%] md:top-[44.63374152871137%] xl:top-[19.577308120133473%]" style={{ position: "absolute" }} />
-
-<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[40.78125%] h-[26.195773081201335%] left-[48.906250000000014%] top-[67.18576195773072%]" style={{ position: "absolute" }} />
+<section className="h-[93.7vh] md:h-[186.8vh] xl:h-[92.4vh]" style={{ position: "relative", backgroundColor: "#dedede" }}>
+      <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[80.63347868217055%] md:w-[35.052083333333336%] h-[13.402271518413066%] md:h-[20.967741935483872%] left-[8.624031007751924%] md:left-[57.41869918699191%] xl:left-[5.833333333333333%] top-[35.771907366943445%] md:top-[28.6391265314262%] xl:top-[19.577308120133473%]" style={{ position: "absolute" }} />
+<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[87.52543604651163%] md:w-[40.78125%] h-[15.090050481783294%] md:h-[26.195773081201335%] left-[4.4186046511627906%] md:left-[48.906250000000014%] top-[71.21283894735888%] md:top-[67.18576195773072%]" style={{ position: "absolute" }} />
+    </section>
+<section className="h-[93.3vh] md:h-[50vh]" style={{ position: "relative", backgroundColor: "#971717" }}>
+      
     </section>
 
     </MainLayout>
