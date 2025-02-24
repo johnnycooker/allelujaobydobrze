a43cfa33d354f35bb342c7ba73532a078f8dea49
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2025-02-24 22:20:07 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,22 +8,22 @@ const GeneratedPage: React.FC = () => {
     <MainLayout>
       
 <section className="h-[93.7vh] md:h-[118.1vh] xl:h-[92.4vh]" style={{ position: "relative", backgroundColor: "#dedede" }}>
-	<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[80.63347868217055%] md:w-[86.63744918699187%] xl:w-[37.552083333333336%] h-[13.402271518413066%] md:h-[25.187941672996253%] xl:h-[36.279873855389646%] left-[7.906976744186048%] md:left-[8.028455284552873%] xl:left-[3.697916666666668%] top-[80.25506648424889%] md:top-[10.062947417174282%] xl:top-[19.930665010592822%] absolute" />
-<div className="w-[85.79941860465117%] md:w-[82.51524390243902%] xl:w-[38.125%] h-[32.72794802657769%] md:h-[32.26783642820736%] xl:h-[50.73027090694936%] left-[5.953246124031039%] md:left-[8.619156504065119%] xl:left-[51.30208333333336%] top-[40.353110648723856%] md:top-[58.756956626747%] xl:top-[22.002355712603073%] absolute">
+	<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[80.63347868217055%] md:w-[60.295985772357724%] xl:w-[37.552083333333336%] h-[13.402271518413066%] md:h-[40.7639785393557%] xl:h-[36.279873855389646%] left-[7.906976744186048%] md:left-[8.028455284552873%] xl:left-[3.697916666666668%] top-[80.25506648424889%] md:top-[10.062947417174282%] xl:top-[19.930665010592822%] absolute" />
+<div className="w-[85.79941860465117%] md:w-[42.27134146341463%] xl:w-[38.125%] h-[32.72794802657769%] md:h-[33.742490806087545%] xl:h-[50.73027090694936%] left-[5.953246124031039%] md:left-[8.619156504065119%] xl:left-[51.30208333333336%] top-[40.353110648723856%] md:top-[58.756956626747%] xl:top-[22.002355712603073%] absolute">
 	  <iframe
 		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206252.80376311584!2d-115.33980749872859!3d36.12516446087101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80beb782a4f57dd1%3A0x3accd5e6d5b379a3!2sLas%20Vegas%2C%20Nevada%2C%20Stany%20Zjednoczone!5e0!3m2!1spl!2spl!4v1740334971548!5m2!1spl!2spl"
 		loading="lazy"
 		className="w-full h-full rounded-[0px] border-0"></iframe>
 	</div>
-<div className="w-[61.62669573643411%] md:w-[53.583587398373986%] xl:w-[21.458333333333332%] h-[9.429129576505254%] md:h-[6.688076623085298%] xl:h-[8.939929328621908%] left-[17.87548449612408%] md:left-[22.563516260162665%] xl:left-[9.21875000000003%] top-[22.646442012123295%] md:top-[41.076488310659144%] xl:top-[75.73616018845715%] absolute">
+<div className="w-[61.62669573643411%] md:w-[35.900660569105696%] xl:w-[21.458333333333332%] h-[9.429129576505254%] md:h-[19.68346832815442%] xl:h-[8.939929328621908%] left-[17.87548449612408%] md:left-[59.343241869918685%] xl:left-[9.21875000000003%] top-[22.646442012123295%] md:top-[65.13178784982959%] xl:top-[75.73616018845715%] absolute">
   <Link href="/asdasd" className="bg-[#095840] hover:bg-[#49bc86] text-[#7a8118] hover:text-[#917878] text-[31px] font-[400] rounded-[80px] border-[2px] border-[#161466] hover:border-[#1a471f] px-4 py-2 cursor-pointer w-full h-full flex items-center justify-center">
     przycisk
   </Link>
 </div>
   </section>
 <section className="h-[93.3vh] md:h-[50vh]" style={{ position: "relative", backgroundColor: "#971717" }}>
-	<div className="w-[42.86458333333333%] h-[74.13043478260869%] left-[4.999999999999997%] top-[10.869565217391303%]" style={{ position: "absolute", backgroundColor: "#f0f0f0" }}>
-	<p className="text-[20px] text-end w-[80.68043742405831%] h-[61.05571847507331%] left-[9.113001215066822%] top-[16.715542521994138%] absolute text-[#176f0b]">Dzięki takiemu podejściu logika generowania responsywnych klas oraz inline stylów została wyizolowana w oddzielnych funkcjach, co ułatwia późniejsze modyfikacje i utrzymanie kodu.</p>
+	<div className="w-[42.86458333333333%] h-[74.13043478260869%] left-[4.999999999999997%] md:left-[52.13541666666676%] xl:left-[4.999999999999997%] top-[10.869565217391303%] md:top-[9.34782608695651%] xl:top-[10.869565217391303%]" style={{ position: "absolute", backgroundColor: "#f0f0f0" }}>
+	<p className="text-[20px] md:text-[11px] xl:text-[20px] text-end md:text-center xl:text-end w-[80.68043742405831%] md:w-[87.23314397676486%] xl:w-[80.68043742405831%] h-[61.05571847507331%] md:h-[67.50733137829911%] xl:h-[61.05571847507331%] left-[9.113001215066822%] md:left-[7.9734000754656815%] xl:left-[9.113001215066822%] top-[16.715542521994138%] md:top-[9.970674486803519%] xl:top-[16.715542521994138%] absolute text-[#176f0b] md:text-[#80235e] xl:text-[#176f0b]">Dzięki takiemu podejściu logika generowania responsywnych klas oraz inline stylów została wyizolowana w oddzielnych funkcjach, co ułatwia późniejsze modyfikacje i utrzymanie kodu.</p>
   </div>
   </section>
 
