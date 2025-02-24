19be4b647e431dc82f571c13ce76209d1fe8ab34
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2025-02-24 18:25:57 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const GeneratedPage: React.FC = () => {
     <MainLayout>
       
 <section className="h-[93.7vh] md:h-[118.1vh] xl:h-[92.4vh]" style={{ position: "relative", backgroundColor: "#dedede" }}>
-      <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[80.63347868217055%] md:w-[86.63744918699187%] xl:w-[37.552083333333336%] h-[13.402271518413066%] md:h-[25.187941672996253%] xl:h-[36.279873855389646%] left-[7.906976744186048%] md:left-[8.028455284552873%] xl:left-[3.697916666666668%] top-[80.25506648424889%] md:top-[10.062947417174282%] xl:top-[19.930665010592822%] absolute" />
+	<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[80.63347868217055%] md:w-[86.63744918699187%] xl:w-[37.552083333333336%] h-[13.402271518413066%] md:h-[25.187941672996253%] xl:h-[36.279873855389646%] left-[7.906976744186048%] md:left-[8.028455284552873%] xl:left-[3.697916666666668%] top-[80.25506648424889%] md:top-[10.062947417174282%] xl:top-[19.930665010592822%] absolute" />
 <div className="w-[85.79941860465117%] md:w-[82.51524390243902%] xl:w-[38.125%] h-[32.72794802657769%] md:h-[32.26783642820736%] xl:h-[50.73027090694936%] left-[5.953246124031039%] md:left-[8.619156504065119%] xl:left-[51.30208333333336%] top-[40.353110648723856%] md:top-[58.756956626747%] xl:top-[22.002355712603073%] absolute">
 	  <iframe
 		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206252.80376311584!2d-115.33980749872859!3d36.12516446087101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80beb782a4f57dd1%3A0x3accd5e6d5b379a3!2sLas%20Vegas%2C%20Nevada%2C%20Stany%20Zjednoczone!5e0!3m2!1spl!2spl!4v1740334971548!5m2!1spl!2spl"
@@ -20,12 +20,12 @@ const GeneratedPage: React.FC = () => {
     przycisk
   </Link>
 </div>
-    </section>
+  </section>
 <section className="h-[93.3vh] md:h-[50vh]" style={{ position: "relative", backgroundColor: "#971717" }}>
-      <div className="w-[42.86458333333333%] h-[74.13043478260869%] left-[4.999999999999997%] top-[10.869565217391303%]" style={{ position: "absolute", backgroundColor: "#f0f0f0" }}>
+	<div className="w-[42.86458333333333%] h-[74.13043478260869%] left-[4.999999999999997%] top-[10.869565217391303%]" style={{ position: "absolute", backgroundColor: "#f0f0f0" }}>
 	
   </div>
-    </section>
+  </section>
 
     </MainLayout>
   );
