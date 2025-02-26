f99ecd060ce47b95077447db7344c23514b074b4
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2025-02-26 11:58:56 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,31 @@
 
 import React from "react";
 import MainLayout from "@/components/layout/MainLayout";
+import Link from "next/link";
 
 const GeneratedPage: React.FC = () => {
   return (
     <MainLayout>
       
 <section className="h-[93.7vh] md:h-[118.1vh] xl:h-[92.4vh]" style={{ position: "relative", backgroundColor: "#dedede" }}>
-      <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[80.63347868217055%] md:w-[87.73501016260164%] xl:w-[37.552083333333336%] h-[13.402271518413066%] md:h-[40.30314904626814%] xl:h-[36.279873855389646%] left-[7.441860465116281%] md:left-[6.686991869918722%] xl:left-[3.697916666666668%] top-[77.00303396392368%] md:top-[10.247279214409303%] xl:top-[19.930665010592822%]" style={{ position: "absolute" }} />
+	<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[80.63347868217055%] md:w-[87.73501016260164%] xl:w-[37.552083333333336%] h-[13.402271518413066%] md:h-[40.30314904626814%] xl:h-[36.279873855389646%] left-[7.441860465116281%] md:left-[6.686991869918722%] xl:left-[3.697916666666668%] top-[77.00303396392368%] md:top-[10.247279214409303%] xl:top-[19.930665010592822%] absolute" />
 <div className="w-[84.17151162790698%] md:w-[42.27134146341463%] xl:w-[43.333333333333336%] h-[31.45036382216422%] md:h-[33.742490806087545%] xl:h-[61.919905771495884%] left-[6.758720930232554%] md:left-[8.619156504065119%] xl:left-[52.03125000000004%] top-[42.21141494605254%] md:top-[58.756956626747%] xl:top-[19.293286219081267%] absolute">
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206252.80376311584!2d-115.33980749872859!3d36.12516446087101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80beb782a4f57dd1%3A0x3accd5e6d5b379a3!2sLas%20Vegas%2C%20Nevada%2C%20Stany%20Zjednoczone!5e0!3m2!1spl!2spl!4v1740334971548!5m2!1spl!2spl"
-      loading="lazy"
-      className="w-full h-full rounded-[0px] border-0"></iframe>
-  </div>
+	  <iframe
+		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206252.80376311584!2d-115.33980749872859!3d36.12516446087101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80beb782a4f57dd1%3A0x3accd5e6d5b379a3!2sLas%20Vegas%2C%20Nevada%2C%20Stany%20Zjednoczone!5e0!3m2!1spl!2spl!4v1740334971548!5m2!1spl!2spl"
+		loading="lazy"
+		className="w-full h-full rounded-[0px] border-0"></iframe>
+	</div>
 <div className="w-[61.62669573643411%] md:w-[35.900660569105696%] xl:w-[21.458333333333332%] h-[9.429129576505254%] md:h-[19.68346832815442%] xl:h-[8.939929328621908%] left-[17.87548449612408%] md:left-[59.343241869918685%] xl:left-[11.093750000000046%] top-[22.646442012123295%] md:top-[65.13178784982959%] xl:top-[71.96702002355724%] absolute">
-    <a href="/asdasd" className="bg-[#095840] hover:bg-[#49bc86] text-[#7a8118] hover:text-[#917878] text-[31px] font-[400] rounded-[80px] border-[2px] border-[#161466] hover:border-[#1a471f] px-4 py-2 cursor-pointer w-full h-full flex items-center justify-center">
-      przycisk
-    </a>
-  </div>
-    </section>
+  <Link href="/asdasd" className="bg-[#095840] hover:bg-[#49bc86] text-[#7a8118] hover:text-[#917878] text-[31px] font-[400] rounded-[80px] border-[2px] border-[#161466] hover:border-[#1a471f] px-4 py-2 cursor-pointer w-full h-full flex items-center justify-center">
+    przycisk
+  </Link>
+</div>
+  </section>
 <section className="h-[93.3vh] md:h-[50vh]" style={{ position: "relative", backgroundImage: "url(https://tropimyprzygody.pl/wp-content/uploads/2023/08/inuici-%E2%80%93-7-najciekawszych-faktow-na-temat-eskimosow-1080x700.jpg)", backgroundSize: "cover", backgroundPosition: "center" }}>
-      <div className="w-[88.44597868217053%] md:w-[46.1572662601626%] xl:w-[42.86458333333333%] h-[32.7068641875095%] md:h-[38.91304347826086%] xl:h-[74.13043478260869%] left-[6.860465116279068%] md:left-[29.20858739837407%] xl:left-[4.999999999999997%] top-[58.24412764446247%] md:top-[56.086956521739104%] xl:top-[10.869565217391303%]" style={{ position: "absolute", backgroundColor: "#f0f0f0", backgroundImage: "url(https://images.immediate.co.uk/production/volatile/sites/63/2024/08/ciekawostki-o-grenlandii-tego-mogliscie-nie-wiedziec-o-najwiekszej-wyspie-swiata1-6095377.jpg?quality=90&resize=980,654)", backgroundSize: "cover", backgroundPosition: "center" }}>
-      <p className="text-[20px] md:text-[11px] xl:text-[20px] text-end md:text-center xl:text-end w-[80.68043742405831%] md:w-[87.23314397676486%] xl:w-[80.68043742405831%] h-[61.05571847507331%] md:h-[67.50733137829911%] xl:h-[61.05571847507331%] top-[16.715542521994138%] md:top-[9.970674486803519%] xl:top-[16.715542521994138%] left-[9.113001215066822%] md:left-[7.9734000754656815%] xl:left-[9.113001215066822%]" style={{ color: "#176f0b", position: "absolute" }}>Dzięki takiemu podejściu logika generowania responsywnych klas oraz inline stylów została wyizolowana w oddzielnych funkcjach, co ułatwia późniejsze modyfikacje i utrzymanie kodu.</p>
-    </div>
-    </section>
+	<div className="w-[88.44597868217053%] md:w-[46.1572662601626%] xl:w-[42.86458333333333%] h-[32.7068641875095%] md:h-[38.91304347826086%] xl:h-[74.13043478260869%] left-[6.860465116279068%] md:left-[29.20858739837407%] xl:left-[4.999999999999997%] top-[58.24412764446247%] md:top-[56.086956521739104%] xl:top-[10.869565217391303%]" style={{ position: "absolute", backgroundColor: "#f0f0f0", backgroundImage: "url(https://images.immediate.co.uk/production/volatile/sites/63/2024/08/ciekawostki-o-grenlandii-tego-mogliscie-nie-wiedziec-o-najwiekszej-wyspie-swiata1-6095377.jpg?quality=90&resize=980,654)", backgroundSize: "cover", backgroundPosition: "center" }}>
+	<p className="text-[20px] md:text-[11px] xl:text-[20px] text-end md:text-center xl:text-end w-[80.68043742405831%] md:w-[87.23314397676486%] xl:w-[80.68043742405831%] h-[61.05571847507331%] md:h-[67.50733137829911%] xl:h-[61.05571847507331%] left-[9.113001215066822%] md:left-[7.9734000754656815%] xl:left-[9.113001215066822%] top-[16.715542521994138%] md:top-[9.970674486803519%] xl:top-[16.715542521994138%] absolute text-[#176f0b] md:text-[#80235e] xl:text-[#176f0b]">Dzięki takiemu podejściu logika generowania responsywnych klas oraz inline stylów została wyizolowana w oddzielnych funkcjach, co ułatwia późniejsze modyfikacje i utrzymanie kodu.</p>
+  </div>
+  </section>
 
     </MainLayout>
   );
